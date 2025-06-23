6fbfbd4e63c1abf94f38300171baeff436275ef7
Cipher0023 <dani.esparza@hotmail.com>
2025-06-23 17:53:26 -0300

    Componente Destaques agnostico à design

--- frontend-regula-e-campo/src/app/page.tsx
+++ frontend-regula-e-campo/src/app/page.tsx
@@ -3,33 +3,29 @@ import Secundário from "@/components/Seçoes/Secundário";
 import Footer from "@/components/footer/footer";
 import Texto from "@/components/notícia/Texto";
 import PropagandaWide from "@/components/propaganda/propagandaWide/PropagandaWide";
-import DestaquesResponsivo from "@/components/notícia/DestaquesResponsivo";
+import Destaques from "@/components/notícia/Destaques";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center gap-y-20">
       <div className="max-w-[1136px] ">
-        <PropagandaWide/>
+        <PropagandaWide />
       </div>
-      
+
       <main className="flex flex-col max-w-[1136px] gap-8 ">
-
-        <DestaquesResponsivo/>
-        <div className="stroke-gray-100 border-b border-t border-gray-100 w-full">
-        </div>
-        <Secundário/>
-        <div className="stroke-gray-100 border-b border-t border-gray-100 w-full">
-        </div>
-        <Novidades/>
+        <Destaques />
+        <div className="stroke-gray-100 border-b border-t border-gray-100 w-full"></div>
+        <Secundário />
+        <div className="stroke-gray-100 border-b border-t border-gray-100 w-full"></div>
+        <Novidades />
         <div className="w-full">
-        <Texto/>
+          <Texto />
         </div>
-
       </main>
-    <Footer/>
-    <div className="fixed bottom-0 left-0 w-full h-16 bg-white border-t border-gray-200 z-50 md:hidden flex items-center justify-center">
-    <span className="text-gray-500">Barra Mobile</span>
-    </div>
+      <Footer />
+      <div className="fixed bottom-0 left-0 w-full h-16 bg-white border-t border-gray-200 z-50 md:hidden flex items-center justify-center">
+        <span className="text-gray-500">Barra Mobile</span>
+      </div>
     </div>
   );
 }
--- frontend-regula-e-campo/src/components/notícia/Destaques.tsx
+++ frontend-regula-e-campo/src/components/notícia/Destaques.tsx
@@ -1,24 +1,36 @@
-import React from 'react'
-import PostGrande from '../postagens/postGrande/PostGrande'
-import PostMedioH from '../postagens/postMedioH/PostMedioH'
+"use client";
+
+import React from "react";
+import PostGrande from "../postagens/postGrande/PostGrande";
+import PostMedioH from "../postagens/postMedioH/PostMedioH";
+import PostMobile from "../postagens/postMobile/PostMobile";
+import { useViewportContext } from "@/contexts/ViewportContext";
 
 function Destaques() {
-  return (
-    <div className="flex flex-row max-h-[548px] 
-      space-x-4">
-      {/* PostGrande ocupa metade do espaço */}
-      <div className="flex-1">
-        <PostGrande />
-      </div>
+  const { isMobile } = useViewportContext();
 
-      {/* Coluna com dois PostMedioH também ocupa metade */}
-      <div className="flex-1 space-y-4 flex flex-col pt-10 pb-10">
-        
-        <PostMedioH />
-        <PostMedioH />  
-      </div>
-    </div>
-  )
+  return (
+    <>
+      {isMobile ? (
+        <div className="flex flex-row w-full max-h-[548px] space-x-4 bg-amber-200">
+          <div className="flex-1">
+            <PostMobile />
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-row max-h-[548px] space-x-4">
+          <div className="flex-1">
+            <PostGrande />
+          </div>
+
+          <div className="flex-1 space-y-4 flex flex-col pt-10 pb-10">
+            <PostMedioH />
+            <PostMedioH />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default Destaques
+export default Destaques;
